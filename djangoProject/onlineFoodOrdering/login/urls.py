b4a85dc51980c6ab6904583c2dc32c25d97fb75b
Sahil Mansuri <sahilmansuri531@gmail.com>
2021-03-13 15:45:10 +0530

    Renders completed and login signup

---
+++ djangoProject/onlineFoodOrdering/login/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('', views.loginPage, name='Login'),
+    path('/signup', views.signupPage, name='Sign Up'),
+    
+]
--- djangoProject/onlineFoodOrdering/onlineFoodOrdering/urls.py
+++ djangoProject/onlineFoodOrdering/onlineFoodOrdering/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
+    path('', include('login.urls')),
     path('foodAdmin', include('app1.urls')),
     path('user', include('users.urls')),
     path('restaurant', include('restaurants.urls')),
