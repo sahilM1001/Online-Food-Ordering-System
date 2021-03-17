2c2cf8d2a71a22dd3b0aa047443e63ce87eb6faa
divyansh <jaindivyansh0818@gmail.com>
2021-03-17 15:02:05 +0530

    ye le

--- djangoProject/onlineFoodOrdering/app1/urls.py
+++ djangoProject/onlineFoodOrdering/app1/urls.py
@@ -5,5 +5,6 @@ urlpatterns=[
     path('', views.adminhomePageView, name='Admin Home'),
     path('/view-users', views.adminUserMGTPageView, name='User Management'),
     path('/view-orders', views.adminOrderMGTPageView, name='Order Management'),
-    path('/view-feedbacks', views.adminFeedbackMGTPageView, name='Feedbacks Management'),
+    path('/view-feedbacks', views.feedbacklisting, name='Feedbacks Management'),
+
 ]
--- djangoProject/onlineFoodOrdering/app1/views.py
+++ djangoProject/onlineFoodOrdering/app1/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.template import loader
 # Create your views here.
+
+import mysql.connector as mcdb
+conn = mcdb.connect(host="localhost", user="root", passwd="", database='onlinefooddeliverysystem')
+print('Successfully connected to database')
+cur = conn.cursor()
 
 #Admin Page Views START HERE
 def adminhomePageView(request):
@@ -15,6 +20,13 @@ def adminOrderMGTPageView(request):
 
 def adminFeedbackMGTPageView(request):
     return render(request, 'admin/feedback.html')
+
+def feedbacklisting(request):
+    cur.execute("SELECT * FROM `tbl_feedbacks`")
+    data = cur.fetchall()
+    #return list(data)
+    print(list(data))
+    return render(request, 'admin/feedback.html', {'feedback': data})
 
 #Admin Page Views END HERE
 
