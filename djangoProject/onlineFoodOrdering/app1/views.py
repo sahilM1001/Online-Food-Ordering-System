596b4d2ae7fcce1b57b09563c9cba4eb8f914be9
Sahil Mansuri <sahilmansuri531@gmail.com>
2021-03-17 13:49:18 +0530

    fixes in admin panel and restaurant

--- djangoProject/onlineFoodOrdering/app1/views.py
+++ djangoProject/onlineFoodOrdering/app1/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+
+##import mysql.connector as mcdb
+##conn = mcdb.connect(host="localhost", user="root", passwd="", database="onlinefooddeliverysystem")
+##print("successfully connected to db")
+
+##cur = conn.cursor()
 # Create your views here.
 
 #Admin Page Views START HERE
@@ -10,10 +16,19 @@ def adminhomePageView(request):
 def adminUserMGTPageView(request):
     return render(request, 'admin/user.html')
 
+def adminUserAddView(request):
+    return render(request, 'admin/addUser.html')
+
 def adminOrderMGTPageView(request):
     return render(request, 'admin/order.html')
 
 def adminFeedbackMGTPageView(request):
+
+    ##cur.execute("SELECT * FROM ` tbl_feedbacks`")
+
+    ##data = cur.fetchall()
+    ##print(list(data))
+    
     return render(request, 'admin/feedback.html')
 
 #Admin Page Views END HERE
